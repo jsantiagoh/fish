3b9446abed5d50fea2d2766fc0b3887b5957f29b
Santiago <santiago@tiqets.com>
2018-12-04 19:11:45 +0100

    Missing dir check

--- conf.d/pyenv.fish
+++ conf.d/pyenv.fish
@@ -1,6 +1,10 @@
 if test -d "$HOME/.pyenv"
     set -x PYENV_ROOT "$HOME/.pyenv"
-    set PATH $PYENV_ROOT/bin $PATH
+
+    if test -d "$HOME/.pyenv/bin"
+        set PATH $PYENV_ROOT/bin $PATH
+    end
+
     status --is-interactive; and source (pyenv init -|psub)
     status --is-interactive; and source (pyenv virtualenv-init -|psub)
     # source /usr/local/opt/asdf/asdf.fish
