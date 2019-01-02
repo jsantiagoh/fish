a9a83f36a033d32bb883f37280d88e5c1bfec42e
Santiago <santiago@tiqets.com>
2019-01-02 18:04:38 +0100

    replace alias with abbr

--- conf.d/pyenv.fish
+++ conf.d/pyenv.fish
@@ -1,11 +1,12 @@
 if test -d "$HOME/.pyenv"
     set -x PYENV_ROOT "$HOME/.pyenv"
 
-    if test -d "$HOME/.pyenv/bin"
+    if test -d "$PYENV_ROOT/bin"
         set PATH $PYENV_ROOT/bin $PATH
     end
 
+    set -x PYENV_VERSION '3.7.1'
+
     status --is-interactive; and source (pyenv init -|psub)
     status --is-interactive; and source (pyenv virtualenv-init -|psub)
-    # source /usr/local/opt/asdf/asdf.fish
 end
